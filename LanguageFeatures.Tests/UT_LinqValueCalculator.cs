88140ce62562be94f93aa67203fac3cc9c6048b9
chisinus <chisinus@gmail.com>
2015-06-01 16:59:34 -0400

    1

--- LanguageFeatures.Tests/UT_LinqValueCalculator.cs
+++ LanguageFeatures.Tests/UT_LinqValueCalculator.cs
@@ -46,5 +46,38 @@ namespace LanguageFeatures.Tests
             //assert
             Assert.AreEqual(goalTotal, result);
         }
+
+        private Product[] CreateProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v == 0))).Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => total * 0.9M);
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+
+            var target = new LinqValueCalculator(mock.Object);
+
+            //act
+            decimal fiveDollarDiscount = target.ValueProducts(CreateProduct(5));
+            decimal tenDollarDiscount = target.ValueProducts(CreateProduct(10));
+            decimal fiftyDollarDiscount = target.ValueProducts(CreateProduct(50));
+            decimal hundredDollarDiscount = target.ValueProducts(CreateProduct(100));
+            decimal fiveHundredDollarDiscount = target.ValueProducts(CreateProduct(500));
+
+            //Assert
+            Assert.AreEqual(5, fiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, tenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, fiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(95, hundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, fiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(CreateProduct(0));
+        }
     }
 }
