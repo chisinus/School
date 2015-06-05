89b906538688edc0427776a7fe4cdc463ebba121
xiajun <chisinus@gmail.com>
2015-06-04 23:25:53 -0400

    1

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -10,15 +10,24 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRespository)
         {
             repository = productRespository;
         }
 
-        public ViewResult List()
+        //public ViewResult List()
+        //{
+        //    return View(repository.Products);
+        //}
+
+        public ViewResult List(int page=1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                                        .OrderBy(p => p.Description)
+                                        .Skip((page - 1) * PageSize)
+                                        .Take(PageSize));
         }
     }
 }
--- SportsStore.WebUI/Infrastracture/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastracture/NinjectDependencyResolver.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -20,17 +21,22 @@ namespace SportsStore.WebUI.Infrastracture
             AddBindings();
         }
 
+        //private void AddBindings()    // Hardcode.
+        //{
+        //    Mock<IProductRepository> mock = new Mock<IProductRepository>();
+        //    mock.Setup(m => m.Products).Returns(new List<Product>
+        //    {
+        //        new Product { Name = "Football", Price = 25 },
+        //        new Product { Name = "Surf board", Price = 179 },
+        //        new Product { Name = "Running shoes", Price = 95 }
+        //    });
+
+        //    kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        //}
+
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
 
         public object GetService(Type serviceType)
