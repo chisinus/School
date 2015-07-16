fa32ac23fe1097e25882e54297d43d7ebca75f71
chisinus <chisinus@gmail.com>
2015-07-16 09:50:16 -0400

    1

---
+++ MyAngular/Controllers/Chapter2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAngular.Controllers
+{
+    public class Chapter2Controller : Controller
+    {
+        // GET: Chapter2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAngular/Scripts/MyAngularJS.js
@@ -0,0 +1,12 @@
+﻿var services = angular.module("Services", []);
+Services.service("Chapter2Service", function ()
+{
+
+});
+
+var controllers = anguler.module("Controllers", ["services"]);
+Controllers.controller("Chapter2Controller", function ($scope, Chapter2Service) {
+
+});
+
+var MyAngularJSApp = angular.module("MyAngularJS", ["controllers", "services"]);
