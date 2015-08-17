851ef48958d95d5ab970440f5cc28244837ac060
xiajun <chisinus@gmail.com>
2015-08-17 12:29:28 +0800

    16

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -13,10 +13,24 @@ namespace UrlsAndRoutes
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            //routes.Add(myRoute);
+
+            //routes.MapRoute("", "X{controller}/{action}");        // with static prefix
+
+            routes.MapRoute("ShopSchema2", "Shop/OldAction", new { controller = "Admin", action = "Index" });        // aliase
+
+            routes.MapRoute("ShopSchema", "Shop/{action}", new { controller = "Home" });   
+
+            //routes.MapRoute("MyRoute", "{controller}/{action}", new { controller = "Home", action = "Index" });   // This overwrites default route
+
+            //routes.MapRoute("", "Public/{controller}/{action}", new { controller = "Home", action = "Index" });   // With static path
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}",      // id is a custom variable
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = "Default ID" }
             );
         }
     }
